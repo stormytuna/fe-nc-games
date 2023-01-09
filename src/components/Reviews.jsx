ef68e8af2816d3eab6bb0720a0ed8989d34253e8
stormytuna <stormytuna@outlook.com>
2023-01-09 15:26:10 +0000

    Add isLoading pattern

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -4,18 +4,24 @@ import { Review } from "./Review";
 
 export function Reviews() {
   const [reviews, setReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     axios
       .get("https://be-nc-games.onrender.com/api/reviews")
       .then((response) => {
         setReviews(response.data.reviews);
+        setIsLoading(false);
       })
       .catch((e) => {
         console.error(e);
         window.alert("ERROR: Could not contact the server, try again later");
       });
   }, []);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
 
   return (
     <div className="Reviews">
