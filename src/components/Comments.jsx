532a7978d7f54ed8102a0a17d8a4aabf011eb3d6
stormytuna <stormytuna@outlook.com>
2023-01-10 11:46:57 +0000

    Refactor add comments feature into Comments component

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -5,6 +5,7 @@ import { Comment } from "./Comment";
 export function Comments({ reviewId }) {
   const [isActive, setIsActive] = useState(false);
   const [comments, setComments] = useState([]);
+  const [commentText, setCommentText] = useState("");
 
   useEffect(() => {
     axios
@@ -18,11 +19,33 @@ export function Comments({ reviewId }) {
       });
   }, [isActive]);
 
+  function handleSubmitComment(e) {
+    e.preventDefault();
+
+    // Hard coding username, in a real life example this would be handled by some authentification system
+    axios
+      .post(`https://be-nc-games.onrender.com/api/reviews/${reviewId}/comments`, { body: commentText, username: "jessjelly" })
+      .then((response) => {
+        // TODO: Come back after ticket 6 is merged and make this add the submitted comment to our comments
+        console.log(response.data.comment);
+      })
+      .catch((e) => {
+        console.error(e);
+        window.alert("ERROR: Could not contact the server, try again later");
+      });
+  }
+
   return (
     <div className="Comments">
       {comments.map((comment) => {
         return <Comment {...comment} />;
       })}
+      <div className="AddComment">
+        <form>
+          <input className="Text" type="text" placeholder="Write a comment..." onChange={(e) => setCommentText(e.target.value)} />
+          <input className="Submit" type="submit" value="Submit" onClick={handleSubmitComment} />
+        </form>
+      </div>
     </div>
   );
 }
