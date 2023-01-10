ccfa5088e9a98185058cc783f2952751e3c817fd
stormytuna <stormytuna@outlook.com>
2023-01-10 11:23:06 +0000

    Add comment submit form (wip)

--- src/components/IndividualReview.jsx
+++ src/components/IndividualReview.jsx
@@ -6,6 +6,7 @@ import { Review } from "./Review";
 export function IndividualReview() {
   const { review_id } = useParams();
   const [review, setReview] = useState({});
+  const [commentText, setCommentText] = useState("");
 
   useEffect(() => {
     axios
@@ -19,9 +20,31 @@ export function IndividualReview() {
       });
   }, []);
 
+  function handleSubmitComment(e) {
+    e.preventDefault();
+
+    // Hard coding username, in a real life example this would be handled by some authentification system
+    axios
+      .post(`https://be-nc-games.onrender.com/api/reviews/${review_id}/comments`, { body: commentText, username: "jessjelly" })
+      .then((response) => {
+        // TODO: Come back after ticket 6 is merged and make this add the submitted comment to our comments
+        console.log(response.data.comment);
+      })
+      .catch((e) => {
+        console.error(e);
+        window.alert("ERROR: Could not contact the server, try again later");
+      });
+  }
+
   return (
     <div className="Reviews">
       <Review {...review} />
+      <div className="AddComment">
+        <form>
+          <input className="Text" type="text" placeholder="Write a comment..." onChange={(e) => setCommentText(e.target.value)} />
+          <input className="Submit" type="submit" value="Submit" onClick={handleSubmitComment} />
+        </form>
+      </div>
     </div>
   );
 }
