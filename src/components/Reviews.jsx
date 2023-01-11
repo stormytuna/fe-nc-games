9112bc3c4f23b2d6eb9627907a19c57e86d1fa63
stormytuna <stormytuna@outlook.com>
2023-01-11 10:11:03 +0000

    Add sort by dropdown

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -2,19 +2,24 @@ import { useEffect, useState } from "react";
 import { getReviews } from "../api";
 import { CategoriesDropdown } from "./CategoriesDropdown";
 import { Review } from "./Review";
+import { SortByDropdown } from "./SortByDropdown";
 
 export function Reviews() {
   const [reviews, setReviews] = useState([]);
   const [chosenCategory, setChosenCategory] = useState("all");
+  const [chosenSortBy, setChosenSortBy] = useState("votes:desc");
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setIsLoading(true);
 
-    const urlParams = {};
+    const urlParams = new URLSearchParams();
     if (chosenCategory !== "all") {
-      urlParams.category = chosenCategory;
+      urlParams.append("category", chosenCategory);
     }
+    const [sortBy, order] = chosenSortBy.split(":");
+    urlParams.append("sort_by", sortBy);
+    urlParams.append("order", order);
 
     getReviews(urlParams)
       .then((reviews) => {
@@ -25,7 +30,7 @@ export function Reviews() {
         console.error(e);
         window.alert("ERROR: Could not contact the server, try again later");
       });
-  }, [chosenCategory]);
+  }, [chosenCategory, chosenSortBy]);
 
   if (isLoading) {
     return <h2>Loading...</h2>;
@@ -34,6 +39,7 @@ export function Reviews() {
   return (
     <div className="Reviews">
       <CategoriesDropdown chosenCategory={chosenCategory} setChosenCategory={setChosenCategory} />
+      <SortByDropdown chosenSortBy={chosenSortBy} setChosenSortBy={setChosenSortBy} />
       <ul>
         {reviews.map((review) => {
           return (
---
+++ src/components/SortByDropdown.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+
+export function SortByDropdown({ chosenSortBy, setChosenSortBy }) {
+  return (
+    <select name="SortBy" value={chosenSortBy} onChange={(e) => setChosenSortBy(e.target.value)}>
+      <option value="created_at:asc">Date ascending</option>
+      <option value="created_at:desc">Date descending</option>
+      <option value="comment_count:asc">Comment count ascending</option>
+      <option value="comment_count:desc">Comment count descending</option>
+      <option value="votes:asc">Votes ascending</option>
+      <option value="votes:desc">Votes descending</option>
+    </select>
+  );
+}
