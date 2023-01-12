88cfad49e5d1f16d8ca878a90b3413bf58c526f7
stormytuna <stormytuna@outlook.com>
2023-01-12 08:57:57 +0000

    Add key prop to comments map

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -36,7 +36,7 @@ export function Comments({ reviewId }) {
   return (
     <div className="Comments">
       {comments.map((comment) => {
-        return <Comment {...comment} setComments={setComments} />;
+        return <Comment key={comment.comment_id} {...comment} setComments={setComments} />;
       })}
       <div className="AddComment">
         <form>
